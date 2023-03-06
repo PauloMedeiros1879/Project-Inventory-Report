753cf487e2a98e9250cc3a0032a1c9ca0e0492f6
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-03-06 09:49:01 -0400

    feat(complete_report.py) Implementa a classe CompleteReport() para retornar todas as informações do relatório completo REQ03

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,24 @@
-class CompleteReport:
-    pass
+from .simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def stocked_products(e, dict):
+        output_stoked = "Produtos estocados por empresa:\n"
+        for enterprise, amount in dict.items():
+            output_stoked += f"- {enterprise}: {amount}\n"
+
+        return output_stoked
+
+    @classmethod
+    def generate(e, parameter):
+        method = super().generate(parameter)
+        products = {}
+        for dict in parameter:
+            if not dict["nome_da_empresa"] in products:
+                products[dict["nome_da_empresa"]] = 1
+            else:
+                products[dict["nome_da_empresa"]] += 1
+
+        method += f"\n{e.stocked_products(products)}"
+        return method
