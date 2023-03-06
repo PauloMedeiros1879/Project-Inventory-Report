2cbf0ee38f926f134db99cd9d2f819aa9919f033
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-03-06 08:03:42 -0400

    feat(simple_report.py) Implementa uma classe que retorna informações do relatório simples dos produtos REQ02

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,36 @@
-class SimpleReport:
-    pass
+from abc import ABC
+from datetime import datetime
+
+
+class SimpleReport(ABC):
+    @classmethod
+    def convert_date(e, date):
+        # mais sobre o método strptime https://www.programiz.com/python-programming/datetime/strptime
+        return datetime.strptime(date, "%Y-%m-%d")
+
+    @classmethod
+    def generate(e, parameter):
+        old = datetime.max
+        nearest = datetime.max
+        products = {}
+        for dic in parameter:
+
+            old = min(old, e.convert_date(dic["data_de_fabricacao"]))
+
+            nearest = (
+                min(nearest, e.convert_date(dic["data_de_validade"]))
+                # date.today() método para obter a data local atual.
+                if e.convert_date(dic["data_de_validade"]) > datetime.today()
+                else nearest
+            )
+
+            if not dic["nome_da_empresa"] in products:
+                products[dic["nome_da_empresa"]] = 1
+            else:
+                products[dic["nome_da_empresa"]] += 1
+
+        return (
+            f"Data de fabricação mais antiga: {old.date()}\n"
+            f"Data de validade mais próxima: {nearest.date()}\n"
+            f"Empresa com mais produtos: {max(products, key=products.get)}"
+        )
