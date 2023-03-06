15bf87042d72ac8e8f7408e33cca54e9ffaeaa25
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-03-06 08:00:43 -0400

    test(test_product.py)  Implementa o teste da função Product() REQ01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,6 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
 # Iniciando projeto! :D
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    id = 0
+    produto = "produto"
+    empresa = "empresa"
+    fabricacao = "fabricacao"
+    validade = "validade"
+    serie = "serie"
+    armazenamento = "armazenamento"
+    inventory = Product(
+        id, produto, empresa, fabricacao, validade, serie, armazenamento
+    )
+    assert inventory.id == id
+    assert inventory.nome_do_produto == produto
+    assert inventory.nome_da_empresa == empresa
+    assert inventory.data_de_fabricacao == fabricacao
+    assert inventory.data_de_validade == validade
+    assert inventory.numero_de_serie == serie
+    assert inventory.instrucoes_de_armazenamento == armazenamento
