6a31290cf0d5da9a9b86bb6c4b327fc21b061a1e
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-03-12 11:51:36 -0400

    test(test_product_report.py) teste de retorno do recurso __repr__ de um objeto Product REQ08

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        12345002,
+        "farinha",
+        "Farinini",
+        "01-05-2021",
+        "02-06-2023",
+        987654321,
+        "ao abrigo de luz",
+    )
+
+    result = product.__repr__()
+
+    assert type(result) == str
+    assert result == (
+        "O produto farinha "
+        "fabricado em 01-05-2021 "
+        "por Farinini com validade "
+        "até 02-06-2023 "
+        "precisa ser armazenado ao abrigo de luz."
+    )
